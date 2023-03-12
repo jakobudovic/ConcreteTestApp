4d83e04a22278287612d258b27da93639697e7c0
jakobudovic <ju8168@student.uni-lj.si>
2023-03-13 00:05:53 +0100

    Add counter with callback event and beautify file

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+function CounterButton({ title, onIncrement }) {
+  return <Button title={title} onPress={onIncrement} />;
+}
 
 export default function App() {
   console.log("App executed!");
   let x = 1;
+  const [count, setCount] = useState(0);
 
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello, world!</Text>
       <StatusBar style="auto" />
+      <CounterButton
+        title={`Click HERE to increment: ${count}`}
+        onIncrement={() => setCount(count + 1)}
+      />
     </View>
   );
 }
@@ -16,14 +27,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
-    color: 'rgb(59,108,212)',
+    color: "rgb(59,108,212)",
     fontSize: 42,
-    fontWeight: '100',
-    textAlign: 'center',
+    fontWeight: "100",
+    textAlign: "center",
   },
-})
+});
