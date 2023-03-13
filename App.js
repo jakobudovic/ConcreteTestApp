382858732963f0af37c266b7f144998e6f6ac209
jakobudovic <ju8168@student.uni-lj.si>
2023-03-13 14:10:48 +0100

    Add scroll view with mock data

--- App.js
+++ App.js
@@ -7,8 +7,43 @@ import {
   ScrollView,
   StyleSheet,
   Text,
+  Image,
   View,
 } from "react-native";
+
+const items = [
+  {
+    id: 1,
+    name: "Concrete 1",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    image: "https://via.placeholder.com/80",
+  },
+  {
+    id: 2,
+    name: "Concrete 2",
+    description: "Nulla tempus vestibulum metus vel tincidunt.",
+    image: "https://via.placeholder.com/80",
+  },
+  {
+    id: 3,
+    name: "Concrete 3",
+    description:
+      "Praesent bibendum, lacus at accumsan efficitur, purus libero aliquam quam.",
+    image: "https://via.placeholder.com/80",
+  },
+];
+
+const Item = ({ item }) => (
+  <View style={styles.itemContainer}>
+    <Image source={{ uri: item.image }} style={styles.itemImage} />
+    <View>
+      <Text style={styles.itemName}>{item.name}</Text>
+      <Text style={styles.itemDescription} numberOfLines={2} width={"50%"}>
+        {item.description}
+      </Text>
+    </View>
+  </View>
+);
 
 export default function App() {
   let x = 1;
@@ -21,16 +56,10 @@ export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <View style={styles.large} />
-        <ScrollView horizontal>
-          <View style={styles.small} />
-          <View style={styles.small} />
-          <View style={styles.small} />
-        </ScrollView>
-        <View style={styles.large} />
-        <View style={styles.small} />
-        <View style={styles.large} />
+      <ScrollView style={styles.container}>
+        {items.map((item) => (
+          <Item key={item.id} item={item} />
+        ))}
       </ScrollView>
     </SafeAreaView>
   );
@@ -39,20 +68,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 10,
+  },
+  itemContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginVertical: 10,
+    padding: 10,
     backgroundColor: "white",
+    borderRadius: 10,
+    elevation: 2,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
   },
-  small: {
-    width: 200,
-    height: 200,
-    marginBottom: 10,
+  itemImage: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
     marginRight: 10,
-    backgroundColor: "skyblue",
   },
-  large: {
-    width: 300,
-    height: 300,
-    marginBottom: 10,
-    marginRight: 10,
-    backgroundColor: "steelblue",
+  itemName: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  itemDescription: {
+    fontSize: 14,
+    color: "#777777",
   },
 });
