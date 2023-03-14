20e070d804ae5c27a8971b1623f900853a9bf34a
jakobudovic <ju8168@student.uni-lj.si>
2023-03-14 12:33:30 +0100

    Implement native stack navigator

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
-import { SafeAreaView } from "react-native";
-import HomeScreen from "./pages/home.js";
+import HomeScreen from "./pages/Home.js";
 
 const queryClient = new QueryClient(); // Initialze the client
+const Stack = createNativeStackNavigator();
 // https://randomuser.me/api?results=10
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <SafeAreaView>
-        <HomeScreen />
-      </SafeAreaView>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </QueryClientProvider>
   );
 }
