798c3021ecbe604226d7a81d92e88a75c1138fb3
jakobudovic <ju8168@student.uni-lj.si>
2023-03-14 02:05:45 +0100

    Clean App.js

--- App.js
+++ App.js
@@ -16,36 +16,7 @@ import {
 const queryClient = new QueryClient(); // Initialze the client
 // https://randomuser.me/api?results=10
 
-const items = [
-  {
-    id: 1,
-    name: "Concrete 1",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    image: "https://via.placeholder.com/80",
-  },
-  {
-    id: 2,
-    name: "Concrete 2",
-    description: "Nulla tempus vestibulum metus vel tincidunt.",
-    image: "https://via.placeholder.com/80",
-  },
-  {
-    id: 3,
-    name: "Concrete 3",
-    description:
-      "Praesent bibendum, lacus at accumsan efficitur, purus libero aliquam quam.",
-    image: "https://via.placeholder.com/80",
-  },
-];
-
 export default function App() {
-  // const getUsers = async () => {
-  //   const res = await fetch("https://randomuser.me/api?results=10");
-  //   return res.json();
-  // };
-
-  // const { data, error, isLoading } = useQuery("randomFacts", getUsers);
-
   return (
     <QueryClientProvider client={queryClient}>
       <SafeAreaView>
@@ -53,13 +24,4 @@ export default function App() {
       </SafeAreaView>
     </QueryClientProvider>
   );
-}
-{
-  /* <SafeAreaView style={styles.container}>
-  <ScrollView style={styles.container}>
-    {items.map((item) => (
-      <Item key={item.id} item={item} />
-    ))}
-  </ScrollView>
-</SafeAreaView> */
 }
